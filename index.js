42040b0065e121bc0928555666012d9648647b71
waleed <94063532+awaleed2556@users.noreply.github.com>
2025-07-01 20:44:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import indexRouter from "./routes/index.js";
 import path, { dirname } from "path";                                            // directory for folder on any machine
 import { fileURLToPath } from "url";
@@ -9,6 +10,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url));                      
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());                                                        // convert the parameters to json when using post
+app.use(cors());
+
+app.use(cors({
+  origin: "https://your-firebase-app.web.app", 
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname,'index.html')));      // at the root route / send the html file            
 app.use('/api',indexRouter);                                                    // middleware
