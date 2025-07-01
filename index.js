e0527cbc10d1117ff750fe8d61f9ab36c948e03c
waleed <94063532+awaleed2556@users.noreply.github.com>
2025-07-01 20:48:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import path, { dirname } from "path";                                           
 import { fileURLToPath } from "url";
 const app = express();
 const PORT = process.env.PORT || 8080;
+const allowedOrigins = [
+  "https://bootloader-kernel-arch.firebaseapp.com",   
+  "https://bootloader-kernel-arch.web.app"            
+];
 
 const __dirname = dirname(fileURLToPath(import.meta.url));                       // get the path for the folder
 
@@ -13,9 +17,14 @@ app.use(express.json());                                                        
 app.use(cors());
 
 app.use(cors({
-  origin: "https://your-firebase-app.web.app", 
-  methods: ["GET", "POST"],
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("CORS not allowed for this origin: " + origin), false);
+    }
+  }
 }));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname,'index.html')));      // at the root route / send the html file            
